8045f94ca389557e14cc9043d5427052001b497b
Rômulo Felipe <romuloflam2011@gmail.com>
2024-08-21 22:03:13 -0300

    first commit

---
+++ book_outlet/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render,  get_object_or_404
+from django.db.models import Avg
+
+from .models import Book
+
+# Create your views here.
+
+def index(request):
+
+    books = Book.objects.all()
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating")) # rating__avg, rating__min
+
+    return render( request, 'book_outlet/index.html', {
+        "books": books
+    })
+
+def book_detail(request, slug):
+    # try:
+  #   book = Book.objects.get(pk=id)
+  # except:
+  #   raise Http404()
+
+   book = get_object_or_404(Book, slug=slug)
+
+   return render( request, 'book_outlet/book_detail.html', {
+    "title": book.title,
+    "author": book.author,
+    "rating": book.rating,
+    "is_bestseller": book.is_bestselling
+    }) 
